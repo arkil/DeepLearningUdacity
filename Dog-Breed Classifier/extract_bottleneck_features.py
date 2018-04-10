a860b2d37ceb5468152efe998b07d16cd4aedd54
thakkararkil3@gmail.com <thakkararkil1996@gmail.com>
2018-04-10 00:46:33 -0400

    Extarct feature from vgg16 or Resnet50 model

---
+++ Dog-Breed Classifier/extract_bottleneck_features.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr  9 22:55:57 2018
+
+@author: Home
+"""
+
+def extract_VGG16(tensor):
+	from keras.applications.vgg16 import VGG16, preprocess_input
+	return VGG16(weights='imagenet', include_top=False).predict(preprocess_input(tensor))
+
+
+def extract_Resnet50(tensor):
+	from keras.applications.resnet50 import ResNet50, preprocess_input
+	return ResNet50(weights='imagenet', include_top=False).predict(preprocess_input(tensor))
